cd5334e9b95ce02cc277e2b551f6f9d70c344eb7
daplaci <davide.placido@cpr.ku.dk>
2022-03-30 15:53:45 -0400

    move under __name__==__main__

--- scripts/ehr_model.py
+++ scripts/ehr_model.py
@@ -23,7 +23,7 @@ args = utils.parse_args()
 #DEFINE FUNCTIONS
 #function to sort the time step as a list
 sort_time = lambda x : list(map(str, sorted(list(map(float,x)), reverse=False)))
-#this function is used to compute the embedding size according to the formula mentioned in the paper 
+#this function is used to compute the embedding size according to the formula mentioned in https://doi.org/10.1038/s41746-018-0029-1
 comput_emb_size = lambda x: int(round(np.sqrt(np.sqrt(x))*6*args.embedding_coeff))
 
 #this Class manage all the methods related to the features
@@ -150,108 +150,95 @@ def get_padd(json_file, pids, percentile):
     print (padd_dict)
     return padd_dict
 
+if __name__=="__main__":
+    print ("Loading json.. ", end='\r')
+    json_file = json.load(open('/home/projects/icu/final_gum/jsons_surv/train_{}.json'.format(args.diag_level), 'r'))
+    json_count = json.load(open('/home/projects/icu/final_gum/jsons_surv/counts_train_{}.json'.format(args.diag_level), 'r'))
+    print ("Json Loaded")
 
-############################################################################################################################################################
-## LOAD JSON THAT INCLUDES THE FEATURES 
-## if the features are in different json - you have to be sure that the time is the same - 
-## otherwise when you merge the different features you will have in the same raw feature belonging to different time point) #######
-############################################################################################################################################################
-print ("Loading json.. ", end='\r')
-json_file = json.load(open('/home/projects/icu/final_gum/jsons_surv/train_{}.json'.format(args.diag_level), 'r'))
-json_count = json.load(open('/home/projects/icu/final_gum/jsons_surv/counts_train_{}.json'.format(args.diag_level), 'r'))
-print ("Json Loaded")
+    if args.save_all:
+        print ("Loading Validation json.. ", end='\r')
+        json_val_file = json.load(open('/home/projects/icu/final_gum/jsons_surv/val_{}.json'.format(args.diag_level), 'r'))
+        print ("Val Json Loaded")
 
-if args.save_all:
-    print ("Loading Validation json.. ", end='\r')
-    json_val_file = json.load(open('/home/projects/icu/final_gum/jsons_surv/val_{}.json'.format(args.diag_level), 'r'))
-    print ("Val Json Loaded")
+    else:
+        json_val_file = None
+        val_pids = None
 
-else:
-    json_val_file = None
-    val_pids = None
+    print ("Loading lpr table..", end='\r')
+    lpr_table = pd.read_csv('/home/projects/icu/final_gum/jsons_surv/outcome.tsv', sep='\t')
+    print ("Lpr Table Loaded")
+    pids_to_failure_time = dict(zip(lpr_table.courseid_unique.astype(str), lpr_table.failure_time))
+    pids = np.asarray([p for p in json_file.keys() if pids_to_failure_time[p] > args.baseline_hour//24]) #filter the patient whose failure time is higher then the baseline
 
+    if args.max_num_patients:
+        #debug mode
+        pids = pids[:args.max_num_patients] 
 
-##########################
-##LOAD whatever file you use for getting the outcome of the patient###
-##########################
-print ("Loading lpr table..", end='\r')
-lpr_table = pd.read_csv('/home/projects/icu/final_gum/jsons_surv/outcome.tsv', sep='\t')
-print ("Lpr Table Loaded")
-pids_to_failure_time = dict(zip(lpr_table.courseid_unique.astype(str), lpr_table.failure_time))
-pids = np.asarray([p for p in json_file.keys() if pids_to_failure_time[p] > args.baseline_hour//24]) #filter the patient whose failure time is higher then the baseline
-if args.max_num_patients:
-    pids = pids[:args.max_num_patients] 
+    print ("Loading metadata..", end='\r')
+    metadata_table = pd.read_csv('/home/projects/icu/final_gum/metadata/metadata_20201114.tsv', sep='\t', usecols=['courseid_unique', 'los_before_icu', 'ageadm'])
+    print ("Metadata Table Loaded")
+    pids_to_los_before_icu = dict(zip(metadata_table.courseid_unique.astype(str), metadata_table.los_before_icu))
+    pids_to_age = dict(zip(metadata_table.courseid_unique.astype(str), metadata_table.ageadm))
+    pids_to_static = {'ageadm':pids_to_age, 'los_before_icu': pids_to_los_before_icu}
 
-print ("Loading metadata..", end='\r')
-metadata_table = pd.read_csv('/home/projects/icu/final_gum/metadata/metadata_20201114.tsv', sep='\t', usecols=['courseid_unique', 'los_before_icu', 'ageadm'])
-print ("Metadata Table Loaded")
-pids_to_los_before_icu = dict(zip(metadata_table.courseid_unique.astype(str), metadata_table.los_before_icu))
-pids_to_age = dict(zip(metadata_table.courseid_unique.astype(str), metadata_table.ageadm))
-pids_to_static = {'ageadm':pids_to_age, 'los_before_icu': pids_to_los_before_icu}
+    breaks = np.array([0] + [int(x) for x in args.n_window.split('-')])  # Make list of integers from e.g ['1-7-14-30-90'] - add 0
+    survivals = create_y_train.make_surv_array((lpr_table.failure_time - args.baseline_hour//24), lpr_table.failure, breaks)
 
-breaks = np.array([0] + [int(x) for x in args.n_window.split('-')])  # Make list of integers from e.g ['1-7-14-30-90'] - add 0
-survivals = create_y_train.make_surv_array((lpr_table.failure_time - args.baseline_hour//24), lpr_table.failure, breaks)
+    pids_to_survival = dict(zip(lpr_table.courseid_unique.astype(str), survivals))
+    y_window = np.asarray([list(pids_to_survival[p]) for p in pids])
 
-pids_to_survival = dict(zip(lpr_table.courseid_unique.astype(str), survivals))
-y_window = np.asarray([list(pids_to_survival[p]) for p in pids])
+    if args.save_all:
+        val_pids = np.asarray([p for p in json_val_file.keys() if pids_to_failure_time[p] > args.baseline_hour//24]) #filter the patient whose failure time is higher then the baseline
+        y_val_window = np.asarray([list(pids_to_survival[p]) for p in val_pids])
 
-if args.save_all:
-    val_pids = np.asarray([p for p in json_val_file.keys() if pids_to_failure_time[p] > args.baseline_hour//24]) #filter the patient whose failure time is higher then the baseline
-    y_val_window = np.asarray([list(pids_to_survival[p]) for p in val_pids])
+    padd_dict = get_padd(json_file, pids, args.padd_percentile)
 
-padd_dict = get_padd(json_file, pids, args.padd_percentile)
+    ######################
+    ## Initialize the different features###
+    ######################
+    time_obj = DomainClass('rel_time', json_file, json_val_file, pids, val_pids)
+    ageadm_obj = DomainClass('ageadm', json_file, json_val_file, pids, val_pids) if args.ageadm else None
+    los_before_icu_obj = DomainClass('los_before_icu', json_file, json_val_file, pids, val_pids) if args.los_before_icu else None
+    diag_obj = DomainClass('diag', json_file, json_val_file, pids, val_pids) if args.diag else None
+    opr_obj = DomainClass('opr', json_file, json_val_file, pids, val_pids) if args.opr else None
+    ube_obj = DomainClass('ube', json_file, json_val_file, pids, val_pids) if args.ube else None
+    chart_obj = DomainClass('chart', json_file, json_val_file, pids, val_pids) if args.chart else None
+    medication_obj = DomainClass('medication', json_file, json_val_file, pids, val_pids) if args.medication else None
+    equipment_obj = DomainClass('equipment', json_file, json_val_file, pids, val_pids) if args.equipment else None
+    biochemical_obj = DomainClass('biochem', json_file, json_val_file, pids, val_pids) if args.biochem else None
 
-######################
-## Initialize the different features###
-######################
-time_obj = DomainClass('rel_time', json_file, json_val_file, pids, val_pids)
-ageadm_obj = DomainClass('ageadm', json_file, json_val_file, pids, val_pids) if args.ageadm else None
-los_before_icu_obj = DomainClass('los_before_icu', json_file, json_val_file, pids, val_pids) if args.los_before_icu else None
-diag_obj = DomainClass('diag', json_file, json_val_file, pids, val_pids) if args.diag else None
-opr_obj = DomainClass('opr', json_file, json_val_file, pids, val_pids) if args.opr else None
-ube_obj = DomainClass('ube', json_file, json_val_file, pids, val_pids) if args.ube else None
-chart_obj = DomainClass('chart', json_file, json_val_file, pids, val_pids) if args.chart else None
-medication_obj = DomainClass('medication', json_file, json_val_file, pids, val_pids) if args.medication else None
-equipment_obj = DomainClass('equipment', json_file, json_val_file, pids, val_pids) if args.equipment else None
-biochemical_obj = DomainClass('biochem', json_file, json_val_file, pids, val_pids) if args.biochem else None
+    #if domain is not None will be added in this list (the domain by default is True unless in the gridsearch you set it as False)
+    list_input_domain = [domain for domain in [time_obj, diag_obj, opr_obj, ube_obj, chart_obj, equipment_obj, medication_obj, biochemical_obj, ageadm_obj, los_before_icu_obj] if domain]
+    [feat.build_vocab() for feat in list_input_domain]
+    [feat.fit_tokenizer() for feat in list_input_domain]
 
-#if domain is not None will be added in this list (the domain by default is True unless in the gridsearch you set it as False)
-list_input_domain = [domain for domain in [time_obj, diag_obj, opr_obj, ube_obj, chart_obj, equipment_obj, medication_obj, biochemical_obj, ageadm_obj, los_before_icu_obj] if domain]
-[feat.build_vocab() for feat in list_input_domain]
-[feat.fit_tokenizer() for feat in list_input_domain]
+    args.cv_split = data_utils.init_cv_split(pids, np.array([pids_to_failure_time[p] for p in pids]), args)
+    args.padd_time = padd_dict['rel_time']
 
-args.cv_split = data_utils.init_cv_split(pids, np.array([pids_to_failure_time[p] for p in pids]), args)
-args.padd_time = padd_dict['rel_time']
+    print ("Changing to dir:{}".format(args.output_dir))
+    os.chdir(args.output_dir)
 
-print ("Changing to dir:{}".format(args.output_dir))
-os.chdir(args.output_dir)
+    df_writer = writer.DF_writer(pids, breaks)
 
-df_writer = writer.DF_writer(pids, breaks)
+    data_extracted = list(map(lambda x: x.extract_data(), list_input_domain))
 
-#uncomment this if you want to use the dictionary - TODO: can be added as a flag
-#[feat.save_dictionary() for feat in list_input_domain]
-data_extracted = list(map(lambda x: x.extract_data(), list_input_domain))
+    for inp, feat, in zip(data_extracted, list_input_domain):
+        feat.inputs = inp
 
-for inp, feat, in zip(data_extracted, list_input_domain):
-    feat.inputs = inp
+    if args.save_all:
+        [feat.save_dictionary() for feat in list_input_domain]
 
-if args.save_all:
-    #save here the input for the model on training
-    np.savez('data/input_training_data_percentile{}_baseline{}_embcoeff{}.npz'.format(args.padd_percentile, args.baseline_hour, args.embedding_coeff), *data_extracted)
-    np.save('data/target_training_data_percentile{}_baseline{}_embcoeff{}.npy'.format(args.padd_percentile, args.baseline_hour, args.embedding_coeff), y_window)
-    np.save('data/pids_training_data_percentile{}_baseline{}_embcoeff{}.npy'.format(args.padd_percentile, args.baseline_hour, args.embedding_coeff), pids)
+        #save here the input for the model on training
+        np.savez('data/input_training_data_percentile{}_baseline{}_embcoeff{}.npz'.format(args.padd_percentile, args.baseline_hour, args.embedding_coeff), *data_extracted)
+        np.save('data/target_training_data_percentile{}_baseline{}_embcoeff{}.npy'.format(args.padd_percentile, args.baseline_hour, args.embedding_coeff), y_window)
+        np.save('data/pids_training_data_percentile{}_baseline{}_embcoeff{}.npy'.format(args.padd_percentile, args.baseline_hour, args.embedding_coeff), pids)
 
-    #save everithing for val
-    val_data_extracted = list(map(lambda x: x.extract_data(dataset='val'), list_input_domain))
-    np.savez('data/input_val_data_percentile{}_baseline{}_embcoeff{}.npz'.format(args.padd_percentile, args.baseline_hour, args.embedding_coeff), *val_data_extracted)
-    np.save('data/target_val_data_percentile{}_baseline{}_embcoeff{}.npy'.format(args.padd_percentile, args.baseline_hour, args.embedding_coeff), y_val_window)
-    np.save('data/pids_val_data_percentile{}_baseline{}_embcoeff{}.npy'.format(args.padd_percentile, args.baseline_hour, args.embedding_coeff), val_pids)
+        #save everithing for val
+        val_data_extracted = list(map(lambda x: x.extract_data(dataset='val'), list_input_domain))
+        np.savez('data/input_val_data_percentile{}_baseline{}_embcoeff{}.npz'.format(args.padd_percentile, args.baseline_hour, args.embedding_coeff), *val_data_extracted)
+        np.save('data/target_val_data_percentile{}_baseline{}_embcoeff{}.npy'.format(args.padd_percentile, args.baseline_hour, args.embedding_coeff), y_val_window)
+        np.save('data/pids_val_data_percentile{}_baseline{}_embcoeff{}.npy'.format(args.padd_percentile, args.baseline_hour, args.embedding_coeff), val_pids)
 
-#    try:
-print ("#### Training window:{}".format(args.window_list))
-learning.train_model(list_input_domain, y_window ,args, df_writer)
-
-#        df_writer.write_progress()
-#    except:
-#        df_writer.write_error()
-
+    print ("#### Training window:{}".format(args.window_list))
+    learning.train_model(list_input_domain, y_window ,args, df_writer)
