1db3dd95d0169ac2097b7ccdf78b6e89313b88d7
daplaci <davide.placido@cpr.ku.dk>
2022-04-06 09:50:16 -0400

    delete metadata files

--- scripts/biochemical/run_biochemical.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python -u generate_file.py --debug 0 --action map_bcc
-python -u generate_file.py --debug 0 --action map_labka
-python -u generate_file.py --debug 0 --action dump_from_pickle
